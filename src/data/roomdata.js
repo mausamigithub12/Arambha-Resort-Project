6e425b2f31468a575f1807a681cd38c3d9167438
mausamigithub12 <mausamiadhikari4@gmail.com>
2026-01-18 15:27:11 +0545

    Add room data and section

---
+++ src/data/roomdata.js
@@ -0,0 +1,125 @@
+ export const detail = [
+  {
+    id: 1,
+    slug: "Single-Room",
+    title: "Single Room",
+    images: [
+      "https://images.unsplash.com/photo-1566665797739-1674de7a421a",
+      "https://images.unsplash.com/photo-1586023492125-27b2c045efd7",
+      "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+      "https://images.unsplash.com/photo-1611892440504-42a792e24d32",
+      "https://images.unsplash.com/photo-1631049307264-da0ec9d70304",
+      "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+    ],
+    desc: "Our Single Bed Room is perfect for solo travelers looking for comfort and peace. It offers a cozy bed, clean space, and a calm environment for rest. Ideal for guests who want privacy and a relaxing stay.",
+    price: "Rs.1200",
+    rating: 4.5,
+    features: ["2 Bed", "Free WiFi", "Desk", "Room Service"],
+    bed: 2,
+     button: "view room detail",
+    reviews:[
+      {
+        name:"Ashitosh Regmi",
+    review:"The single room was clean, comfortable, and very peaceful. Perfect for a solo stay.",
+     },
+     {
+name:"manara sharma",
+review:" I loved the calm environment and cozy bed. It was a relaxing experience."
+}
+
+],},
+  {
+    id: 2,
+    slug: "Couple-Room",
+    title: "Couple Room",
+    images: [
+      "https://images.unsplash.com/photo-1566665797739-1674de7a421a",
+      "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+      "https://images.unsplash.com/photo-1586023492125-27b2c045efd7",
+      "https://images.unsplash.com/photo-1611892440504-42a792e24d32",
+      "https://images.unsplash.com/photo-1631049307264-da0ec9d70304",
+      "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+    ],
+    desc: "The Double Room is designed for couples and friends traveling together. It features a comfortable bed, warm interiors, and a peaceful atmosphere. A perfect choice for a romantic and comfortable stay at Arambha Resort.",
+    price: "Rs.3000",
+    rating: 4.8,
+    features: ["King Size Bed", "Balcony View", "1 Bed", "Spa Access"],
+    bed: 1,
+     button: "view room detail",
+        reviews:[
+      {
+        name:"ritu rana",
+    review:"The single room was clean, comfortable, and very peaceful. Perfect for a solo stay.",
+     },
+     {
+name:"riva sharma",
+review:" I loved the calm environment and cozy bed. It was a relaxing experience."
+}
+
+]
+   
+  },
+  {
+    id: 3,
+    slug: "Family-Room",
+    title: "Family Room",
+    images: [
+      "https://images.unsplash.com/photo-1611892440504-42a792e24d32",
+      "https://images.unsplash.com/photo-1566665797739-1674de7a421a",
+      "https://images.unsplash.com/photo-1586023492125-27b2c045efd7",
+      "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+      "https://images.unsplash.com/photo-1631049307264-da0ec9d70304",
+      "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+    ],
+    desc: "Our Family Room is spacious and ideal for large families or group stays. It comes with 5 to 6 comfortable beds and enough space to relax together. Perfect for family vacations, gatherings, and group trips.",
+    price: "Rs.4000",
+    rating: 4.7,
+    features: ["5-6 Beds", "Living Area", "Kitchenette", "Kids Play Zone"],
+    bed: 6,
+     button: "view room detail",
+      reviews:[
+      {
+        name:"hitesh thapa",
+    review:"The single room was clean, comfortable, and very peaceful. Perfect for a solo stay.",
+     },
+     {
+name:"rishi sharma",
+review:" I loved the calm environment and cozy bed. It was a relaxing experience."
+}
+
+]
+
+
+  },
+  {
+    id: 4,
+    slug: "Premium-Room",
+    title: "Premium Room",
+    images: [
+      "https://images.unsplash.com/photo-1631049307264-da0ec9d70304",
+      "https://images.unsplash.com/photo-1611892440504-42a792e24d32",
+      "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+      "https://images.unsplash.com/photo-1566665797739-1674de7a421a",
+      "https://images.unsplash.com/photo-1586023492125-27b2c045efd7",
+      "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+    ],
+    desc: "Our Premium Room offers extra comfort with a spacious and elegant design. It features a cozy bed, modern amenities, and a peaceful atmosphere. Perfect for guests who want a relaxed, stylish, and memorable stay at Arambha Resort.",
+    price: "Rs.4500",
+    rating: 4.9,
+    features: ["Suite Layout", "Private Jacuzzi", "1 Bed", "Premium View"],
+    bed: 1,
+     button: "view room detail",
+        reviews:[
+      {
+        name:"rakhsya siwakoti",
+    review:"The premium room was clean, comfortable, and very peaceful. Perfect for a solo stay.",
+    
+     },
+     {
+name:"apekshya bhusal",
+review:" I loved the calm environment and cozy bed. It was a relaxing experience."
+}
+
+]
+  },
+];
